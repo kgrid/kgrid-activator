ed03b90a6041b87296546e12db93aff8d3bdc5ac
nggittle <nathaniel.gittlen@gmail.com>
2017-03-23 14:53:03 -0400

    Added standard equals and hashcode methods for testing. -ng

--- src/main/java/org/uofm/ot/executionStack/transferObjects/Result.java
+++ src/main/java/org/uofm/ot/executionStack/transferObjects/Result.java
@@ -60,5 +60,36 @@ public class Result {
 				+ "]";
 	}
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
 
+		Result result1 = (Result) o;
+
+		if (success != result1.success) {
+			return false;
+		}
+		if (result != null ? !result.equals(result1.result) : result1.result != null) {
+			return false;
+		}
+		if (errorMessage != null ? !errorMessage.equals(result1.errorMessage)
+				: result1.errorMessage != null) {
+			return false;
+		}
+		return source != null ? source.equals(result1.source) : result1.source == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result1 = success;
+		result1 = 31 * result1 + (result != null ? result.hashCode() : 0);
+		result1 = 31 * result1 + (errorMessage != null ? errorMessage.hashCode() : 0);
+		result1 = 31 * result1 + (source != null ? source.hashCode() : 0);
+		return result1;
+	}
 }
