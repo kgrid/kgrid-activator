63445babba238bb644a9bee8c110280b16b89c46
nggittle <nathaniel.gittlen@gmail.com>
2017-03-24 16:51:57 -0400

    Created new class for citation metadata. -ng

---
+++ src/main/java/org/uofm/ot/executionStack/transferObjects/Citation.java
@@ -0,0 +1,29 @@
+package org.uofm.ot.executionStack.transferObjects;
+
+/**
+ * Created by nggittle on 3/24/2017.
+ */
+public class Citation {
+
+  private String title;
+
+  private String at;
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getAt() {
+    return at;
+  }
+
+  public void setAt(String at) {
+    this.at = at;
+  }
+
+
+}
