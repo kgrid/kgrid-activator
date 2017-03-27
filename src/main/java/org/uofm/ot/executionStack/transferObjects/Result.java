adc3aad741e17f7da43e61c82a8cfa9536e01a6a
ngittlen <nathaniel.gittlen@gmail.com>
2017-03-27 15:57:00 -0400

    Fixing broken file. -ng

--- src/main/java/org/uofm/ot/executionStack/transferObjects/Result.java
+++ src/main/java/org/uofm/ot/executionStack/transferObjects/Result.java
@@ -2,29 +2,43 @@ package org.uofm.ot.executionStack.transferObjects;
 
 public class Result {
 
+	@Deprecated
 	private int success;
+
+	@Deprecated
+	private String errorMessage;
 
 	private Object result;
 
-	private String errorMessage;
-
 	private String source;
-		public Result(){}
 
-	public Result(int success, String result, String errorMessage) {
+	private Metadata metadata;
+
+	public Result(){}
+
+	public Result(String result) {
 		super();
-		this.success = success;
 		this.result = result;
-		this.errorMessage = errorMessage;
 	}
 
-
+	@Deprecated
 	public int getSuccess() {
 		return success;
 	}
 
+	@Deprecated
 	public void setSuccess(int success) {
 		this.success = success;
+	}
+
+	@Deprecated
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	@Deprecated
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
 	}
 
 	public Object getResult() {
@@ -35,20 +49,20 @@ public class Result {
 		this.result = result;
 	}
 
-	public String getErrorMessage() {
-		return errorMessage;
-	}
-
-	public void setErrorMessage(String errorMessage) {
-		this.errorMessage = errorMessage;
-	}
-
 	public String getSource() {
 		return source;
 	}
 
 	public void setSource(String source) {
 		this.source = source;
+	}
+
+	public Metadata getMetadata() {
+		return metadata;
+	}
+
+	public void setMetadata(Metadata metadata) {
+		this.metadata = metadata;
 	}
 
 	@Override
@@ -60,5 +74,36 @@ public class Result {
 				+ "]";
 	}
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
 
+		Result result1 = (Result) o;
+
+		if (success != result1.success) {
+			return false;
+		}
+		if (result != null ? !result.equals(result1.result) : result1.result != null) {
+			return false;
+		}
+		if (errorMessage != null ? !errorMessage.equals(result1.errorMessage)
+				: result1.errorMessage != null) {
+			return false;
+		}
+		return source != null ? source.equals(result1.source) : result1.source == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result1 = success;
+		result1 = 31 * result1 + (result != null ? result.hashCode() : 0);
+		result1 = 31 * result1 + (errorMessage != null ? errorMessage.hashCode() : 0);
+		result1 = 31 * result1 + (source != null ? source.hashCode() : 0);
+		return result1;
+	}
 }
