83c27aa8b15daf4f4b3939b0ef35c6c6d1e2ce0f
ngittlen <nathaniel.gittlen@gmail.com>
2017-03-29 13:42:59 -0400

    Implementing new ko builder. -ng

--- src/test/java/org/uofm/ot/executionStack/controller/ExecutionControllerTest.java
+++ src/test/java/org/uofm/ot/executionStack/controller/ExecutionControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.uofm.ot.executionStack.ObjectTellerExecutionStackApplication;
 import org.uofm.ot.executionStack.exception.OTExecutionStackException;
+import org.uofm.ot.executionStack.transferObjects.KnowledgeObjectBuilder;
 import org.uofm.ot.executionStack.transferObjects.KnowledgeObjectDTO;
 import org.uofm.ot.executionStack.transferObjects.Result;
 
@@ -47,6 +48,7 @@ public class ExecutionControllerTest {
   @Test(expected = NullPointerException.class)
   public void testCalculateWithNullInputsAndNullKO() throws Exception {
     ex.validateAndExecute(null, null);
+    assertNotNull(null);
   }
 
   @Test(expected = OTExecutionStackException.class)
@@ -58,37 +60,42 @@ public class ExecutionControllerTest {
 
   @Test(expected = OTExecutionStackException.class)
   public void testCalculateWithBadKOandInputs() throws Exception {
-    KnowledgeObjectDTO ko = new KnowledgeObjectDTO();
+    KnowledgeObjectDTO ko = new KnowledgeObjectBuilder()
+        .inputMessage(INPUT_SPEC)
+        .outputMessage(OUTPUT_SPEC)
+        .build();
     Map<String, Object> inputs = new HashMap<>();
     inputs.put("test", "test");
-    ko.inputMessage = INPUT_SPEC;
-    ko.outputMessage = OUTPUT_SPEC;
     assertNotNull(ex.validateAndExecute(inputs, ko));
   }
 
   @Test(expected = OTExecutionStackException.class)
   public void testCalculateWithExpectedBadOutput() {
-    KnowledgeObjectDTO ko = new KnowledgeObjectDTO();
+    KnowledgeObjectDTO ko = new KnowledgeObjectBuilder()
+        .inputMessage(INPUT_SPEC)
+        .outputMessage(OUTPUT_SPEC)
+        .build();
     Map<String, Object> inputs = new HashMap<>();
     inputs.put("test", "test");
     Result expectedResult = new Result(null);
     expectedResult.setSource(null);
-    ko.inputMessage = INPUT_SPEC;
-    ko.outputMessage = OUTPUT_SPEC;
     Result generatedResult = ex.validateAndExecute(inputs, ko);
     assertEquals(expectedResult, generatedResult);
   }
 
   @Test
   public void testCalculateWithExpectedOkOutput() {
-    KnowledgeObjectDTO ko = new KnowledgeObjectDTO();
+    KnowledgeObjectDTO ko = new KnowledgeObjectBuilder()
+        .inputMessage(INPUT_SPEC)
+        .outputMessage(OUTPUT_SPEC)
+        .payloadContent("def execute():return true")
+        .payloadEngineType("PYTHON")
+        .payloadFunctionName("execute")
+        .build();
     Map<String, Object> inputs = new HashMap<>();
     inputs.put("rxcui", "1723222 2101 10767");
     Result expectedResult = new Result(null);
     expectedResult.setSource(null);
-    ko.inputMessage = INPUT_SPEC;
-    ko.outputMessage = OUTPUT_SPEC;
-    ko.genPayload("def execute():return true", "PYTHON", "execute");
     Result generatedResult = ex.validateAndExecute(inputs, ko);
     assertEquals(expectedResult, generatedResult);
   }
