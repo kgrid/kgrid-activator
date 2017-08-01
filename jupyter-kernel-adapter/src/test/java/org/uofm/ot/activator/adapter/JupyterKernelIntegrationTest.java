24c267a3a5905c56f52e77d2e41846fc86d7d3d7
Colin Gross <grosscol@gmail.com>
2017-08-01 11:43:37 -0400

    Externalize configuration. Update tests to use spring to inject adapter. Provide internal application.properties defaults

--- jupyter-kernel-adapter/src/test/java/org/uofm/ot/activator/adapter/JupyterKernelIntegrationTest.java
+++ jupyter-kernel-adapter/src/test/java/org/uofm/ot/activator/adapter/JupyterKernelIntegrationTest.java
@@ -9,56 +9,64 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.uofm.ot.activator.exception.OTExecutionStackException;
 
 /**
  * Created by grosscol on 2017-06-20.
  */
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {JupyterKernelAdapter.class})
+@TestPropertySource("classpath:application.properties")
 @Category(org.uofm.ot.activator.adapter.IntegrationTest.class)
 public class JupyterKernelIntegrationTest {
 
   @Rule
   public ExpectedException expectedEx = ExpectedException.none();
 
+  @Autowired
+  JupyterKernelAdapter adapter;
+
   @Test
   public void runHello() throws Exception {
     String code = TestUtils.loadFixture("payload-hello-no-params.py");
-    JupyterKernelAdapter adapter = new JupyterKernelAdapter();
     Object result = adapter.execute(new HashMap<>(), code, "hello", Map.class);
-    assertThat(((Map) result).get("Hello"), equalTo("World") );
+    assertThat(((Map) result).get("Hello"), equalTo("World"));
   }
 
   @Test
   public void runNumbers() throws Exception {
     String code = TestUtils.loadFixture("payload-numbers.py");
-    HashMap<String,Object> args = new HashMap<>();
-    int[] val = new int[]{0,1,2,3,4,5};
-    args.put("inputs", val );
+    HashMap<String, Object> args = new HashMap<>();
+    int[] val = new int[]{0, 1, 2, 3, 4, 5};
+    args.put("inputs", val);
 
-    JupyterKernelAdapter adapter = new JupyterKernelAdapter();
     Object result = adapter.execute(args, code, "numbers", Map.class);
-    assertThat(((Map) result).get("sum"), equalTo(15) );
+    assertThat(((Map) result).get("sum"), equalTo(15));
   }
 
   @Test
   public void runBadSyntax() throws Exception {
     String code = TestUtils.loadFixture("payload-syntax-error.py");
-    JupyterKernelAdapter adapter = new JupyterKernelAdapter();
 
     expectedEx.expect(OTExecutionStackException.class);
     expectedEx.expectMessage("Error in exec environment: SyntaxError");
 
-    Object result = adapter.execute(new HashMap<>(), code, "bad_function", Map.class);
+    adapter.execute(new HashMap<>(), code, "bad_function", Map.class);
   }
 
   @Test
   public void runRuntimeError() throws Exception {
     String code = TestUtils.loadFixture("payload-runtime-error.py");
-    JupyterKernelAdapter adapter = new JupyterKernelAdapter();
 
     expectedEx.expect(OTExecutionStackException.class);
     expectedEx.expectMessage("Error in exec environment");
 
     Object result = adapter.execute(new HashMap<>(), code, "bad_function", Map.class);
+
   }
 }
