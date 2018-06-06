24604b0b7eb2ebba40f237fe53ecaad31c3b8244
Greg Farris <greg.t.farris@gmail.com>
2018-06-05 22:22:57 -0400

    shelf default, missed endpoint decorator change, changed heroku initial data, it is using test data to seed shelf on heroku

--- src/main/java/org/kgrid/activator/services/ActivationService.java
+++ src/main/java/org/kgrid/activator/services/ActivationService.java
@@ -30,7 +30,7 @@ public class ActivationService {
   private HashMap<String, Adapter> adapters;
   private long knowledgeObjectsFound;
 
-  @Value("${kgrid.shelf.cdostore.filesystem.location}")
+  @Value("${kgrid.shelf.cdostore.filesystem.location:shelf}")
   private String locationStoragePath;
 
   @Autowired
