bc1d3bb72e57afd39ac76dd420e008e21296a156
Greg Farris <greg.t.farris@gmail.com>
2018-08-08 14:59:44 -0400

    updated to load and deploy cancer risk to heroku

--- .circleci/create_shelf.sh
+++ .circleci/create_shelf.sh
@@ -4,6 +4,8 @@
  .circleci/download_assets.sh https://api.github.com/repos/kgrid/CPIC-objects/releases/latest
  .circleci/download_assets.sh https://api.github.com/repos/kgrid/mopen-opioid-collection/releases/latest
  .circleci/download_assets.sh https://api.github.com/repos/kgrid/kgrid-activator/releases/latest
+ .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/cancer-risk/releases/latest
  unzip -o opioid-all.zip -d shelf
  unzip -o cpic-all.zip -d shelf
  unzip -o hello-world.zip -d shelf
+ unzip -o cancer-risk.zip -d shelf
