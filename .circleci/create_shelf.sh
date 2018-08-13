365331fd3dbcdd6b0e2aefefb8b2782fbbf3a6c5
Greg Farris <greg.t.farris@gmail.com>
2018-08-13 11:33:15 -0400

    update release ko location for heroku release

--- .circleci/create_shelf.sh
+++ .circleci/create_shelf.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
  mkdir shelf
- .circleci/download_assets.sh https://api.github.com/repos/kgrid/CPIC-objects/releases/latest
- .circleci/download_assets.sh https://api.github.com/repos/kgrid/mopen-opioid-collection/releases/latest
- .circleci/download_assets.sh https://api.github.com/repos/kgrid/kgrid-activator/releases/latest
+ .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/cpic-objects/releases/latest
+ .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/mopen-opioid-collection/releases/latest
+ .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/kgrid-activator/releases/latest
  .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/cancer-risk/releases/latest
  unzip -o opioid-all.zip -d shelf
  unzip -o cpic-all.zip -d shelf
