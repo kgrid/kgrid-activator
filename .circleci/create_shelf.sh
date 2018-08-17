2c29a704339a02d8a84e3f8df0a4f91b7a35d8f9
gqmeng <gqmeng.meng@gmail.com>
2018-08-17 15:35:39 -0400

    Update create_shelf.sh
    Added script-numerate KOs for CI release and deploy

--- .circleci/create_shelf.sh
+++ .circleci/create_shelf.sh
@@ -3,9 +3,11 @@
  mkdir shelf
  .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/cpic-objects/releases/latest
  .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/mopen-opioid-collection/releases/latest
- .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/kgrid-activator/releases/latest
+ .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/example-projects/releases/latest
  .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/cancer-risk/releases/latest
+ .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/script-numerate/releases/latest
  unzip -o opioid-all.zip -d shelf
  unzip -o cpic-all.zip -d shelf
  unzip -o hello-world.zip -d shelf
  unzip -o cancer-risk.zip -d shelf
+ unzip -o scriptnuemrate-all.zip -d shelf
