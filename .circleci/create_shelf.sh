9e10f39760bf92815a17d9dafdc9ed3d1d42a5a1
Greg Farris <greg.t.farris@gmail.com>
2018-09-13 14:33:27 -0400

    Update actuator endpoint access

--- .circleci/create_shelf.sh
+++ .circleci/create_shelf.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
- mkdir shelf
- .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/cpic-objects/releases/latest
- .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/mopen-opioid-collection/releases/latest
- .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/example-projects/releases/latest
- .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/cancer-risk/releases/latest
- .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/icon-array/releases/latest
- .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/script-numerate/releases/latest
- .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/postpci/releases/latest
-  .circleci/download_assets.sh https://api.github.com/repos/kgrid-objects/labwise/releases/latest
+ mkdir application/shelf
+ repos=(mopen-opioid-collection cpic-objects example-kos cancer-risk icon-array script-numerate postpci labwise )
+
+ for i in "${repos[@]}"
+ do
+  url=(https://api.github.com/repos/kgrid-objects/$i/releases/latest)
+  .circleci/download_assets.sh "$url"
+ done
+
  unzip -o opioid-all.zip -d shelf
  unzip -o cpic-all.zip -d shelf
  unzip -o hello-world.zip -d shelf
