c976f6e09a84cbb8f4cc8ec76425baff39834599
Greg Farris <greg.t.farris@gmail.com>
2018-10-03 17:27:10 -0400

    Updated to support tagging standard

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -2,10 +2,10 @@
 
  git config --global user.email "$GIT_USER_EMAIL"
  git config --global user.name "$GIT_USER_NAME"
- TAG_NAME="kgrid-activator-$RELEASE"
+ TAG_NAME="v$RELEASE"
  mvn release:clean release:prepare -B  -DscmCommentPrefix="Release [skip ci] " -DreleaseVersion=$RELEASE -DdevelopmentVersion=$NEXT release:perform -P ci -s .circleci/settings.xml
  curl -X POST "https://api.github.com/repos/kgrid/kgrid-activator/releases?access_token=$GITHUB_TOKEN" \
-         -H 'Content-Type: application/json' -d "{\"tag_name\":\"$TAG_NAME\"}"
+         -H 'Content-Type: application/json' -d "{\"tag_name\":\"$TAG_NAME\", \"name\":\"KGrid Activator $TAG_NAME\"}"
  RELEASE_ID=$(curl -s https://api.github.com/repos/kgrid/kgrid-activator/releases/latest | jq -r ".id")
          curl -X POST "https://uploads.github.com/repos/kgrid/kgrid-activator/releases/$RELEASE_ID/assets?access_token=$GITHUB_TOKEN&name=$TAG_NAME.jar" \
            -H "Content-Type: application/octet-stream" \
