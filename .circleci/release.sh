1e87832ff6cddc6f12c89ac50f3eb16a78133a6d
circleci <kgrid-developers@umich.edu>
2018-10-05 13:03:37 -0400

    Alter release process to tag artifacts correctly

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -2,12 +2,13 @@
 
  git config --global user.email "$GIT_USER_EMAIL"
  git config --global user.name "$GIT_USER_NAME"
- TAG_NAME="v$RELEASE"
- mvn release:clean release:prepare -B  -DscmCommentPrefix="Release [skip ci] " -DreleaseVersion=$RELEASE -DdevelopmentVersion=$NEXT release:perform -P ci -s .circleci/settings.xml
+ TAG_NAME="$RELEASE"
+ FILE_NAME="kgrid-activator-$TAG_NAME.jar"
+ mvn release:clean release:prepare -B -Dtag=$TAG_NAME -DscmCommentPrefix="Release [skip ci] " -DreleaseVersion=$RELEASE -DdevelopmentVersion=$NEXT release:perform -P ci -s .circleci/settings.xml
  curl -X POST "https://api.github.com/repos/kgrid/kgrid-activator/releases?access_token=$GITHUB_TOKEN" \
          -H 'Content-Type: application/json' -d "{\"tag_name\":\"$TAG_NAME\", \"name\":\"KGrid Activator $TAG_NAME\"}"
  RELEASE_ID=$(curl -s https://api.github.com/repos/kgrid/kgrid-activator/releases/latest | jq -r ".id")
-         curl -X POST "https://uploads.github.com/repos/kgrid/kgrid-activator/releases/$RELEASE_ID/assets?access_token=$GITHUB_TOKEN&name=$TAG_NAME.jar" \
+         curl -X POST "https://uploads.github.com/repos/kgrid/kgrid-activator/releases/$RELEASE_ID/assets?access_token=$GITHUB_TOKEN&name=$FILE_NAME" \
            -H "Content-Type: application/octet-stream" \
-           --data-binary "@target/$TAG_NAME.jar"
+           --data-binary "@target/$FILE_NAME"
 
