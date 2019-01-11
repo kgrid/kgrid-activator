2f132ec5ef1759050aeef692e076659b18d546ec
circleci <kgrid-developers@umich.edu>
2019-01-11 16:23:04 -0500

    Getting setup for Release Cadidate build, removed snapshot depends and updated CI release processing

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -4,11 +4,19 @@
  git config --global user.name "$GIT_USER_NAME"
  TAG_NAME="$RELEASE"
  FILE_NAME="kgrid-activator-$TAG_NAME.jar"
- mvn release:clean release:prepare -B -Dtag=$TAG_NAME -DscmCommentPrefix="Release [skip ci] " -DreleaseVersion=$RELEASE -DdevelopmentVersion=$NEXT release:perform -P ci -s .circleci/settings.xml
+
+ mvn release:clean release:prepare -B -Dtag=$TAG_NAME -DscmCommentPrefix="Release [skip ci] " \
+           -DreleaseVersion=$RELEASE -DdevelopmentVersion=$NEXT -Darguments="-Ddockerfile.tag=$TAG_NAME" release:perform -P ci -s .circleci/settings.xml
+
  curl -X POST "https://api.github.com/repos/kgrid/kgrid-activator/releases?access_token=$GITHUB_TOKEN" \
-         -H 'Content-Type: application/json' -d "{\"tag_name\":\"$TAG_NAME\", \"name\":\"KGrid Activator $TAG_NAME\"}"
+         -H 'Content-Type: application/json' -d "{\"tag_name\":\"$TAG_NAME\", \"name\":\"KGrid Activator $TAG_NAME\", \"prerelease\":$PRERELEASE}"
+
  RELEASE_ID=$(curl -s https://api.github.com/repos/kgrid/kgrid-activator/releases/latest | jq -r ".id")
-         curl -X POST "https://uploads.github.com/repos/kgrid/kgrid-activator/releases/$RELEASE_ID/assets?access_token=$GITHUB_TOKEN&name=$FILE_NAME" \
+
+ curl -X POST "https://uploads.github.com/repos/kgrid/kgrid-activator/releases/$RELEASE_ID/assets?access_token=$GITHUB_TOKEN&name=$FILE_NAME" \
            -H "Content-Type: application/octet-stream" \
            --data-binary "@target/$FILE_NAME"
+
+
+
 
