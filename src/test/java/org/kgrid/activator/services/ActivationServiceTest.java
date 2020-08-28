b7bfc93ea98d10cb4fe33d4385f051f3d8ad3802
Adam G Beck <mradamgbeck@gmail.com>
2020-08-28 13:34:54 -0400

    Remove dash ark (#72)
    * Refactoring ark id
    
    * Created unit test for activation service that tests everything EndpointActivation and EndpointExecution tests were testing in less code
    
    Co-authored-by: ngittlen <nggittle@med.umich.edu>

---
+++ src/test/java/org/kgrid/activator/services/ActivationServiceTest.java
@@ -0,0 +1,168 @@
+package org.kgrid.activator.services;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kgrid.activator.ActivatorException;
+import org.kgrid.activator.EndPointResult;
+import org.kgrid.adapter.api.Adapter;
+import org.kgrid.adapter.api.AdapterException;
+import org.kgrid.adapter.v8.V8Executor;
+import org.kgrid.shelf.ShelfResourceNotFound;
+import org.kgrid.shelf.domain.ArkId;
+import org.kgrid.shelf.repository.KnowledgeObjectRepository;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.kgrid.activator.utils.KoCreationTestHelper.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ActivationServiceTest {
+
+    public static final String OBJECT_LOCATION = "ObjectLocation";
+    private EndpointId endpointId1;
+    private Endpoint endpoint1 = Mockito.mock(Endpoint.class);
+    private Map<EndpointId, Endpoint> endpointMap = new HashMap<>();
+    @Mock
+    private AdapterResolver adapterResolver;
+    @Mock
+    private KnowledgeObjectRepository koRepo;
+    @Mock
+    private Adapter adapter;
+    @Mock
+    private V8Executor executor;
+    private ActivationService activationService;
+    private JsonNode deploymentJson;
+    private JsonNode metadata;
+    private String input = "input";
+
+
+    @Before
+    public void setup() throws IOException {
+        endpointId1 = new EndpointId(ARK_ID, ENDPOINT_NAME);
+        endpointMap.put(endpointId1, endpoint1);
+        activationService = new ActivationService(adapterResolver, endpointMap, koRepo);
+        deploymentJson = getEndpointDeploymentJson();
+        metadata = generateMetadata();
+
+        when(endpoint1.getDeployment()).thenReturn(deploymentJson);
+        when(endpoint1.getExecutor()).thenReturn(executor);
+        when(adapterResolver.getAdapter(ADAPTER)).thenReturn(adapter);
+        when(adapter.activate(any(), any(), any(), any())).thenReturn(executor);
+        when(koRepo.getObjectLocation(ARK_ID)).thenReturn(OBJECT_LOCATION);
+        when(endpoint1.getMetadata()).thenReturn(metadata);
+    }
+
+    @Test
+    public void activateGetsDeploymentFromEndpoint() {
+        activationService.activate(endpointMap);
+        verify(endpoint1).getDeployment();
+    }
+
+    @Test
+    public void activateGetsAdapterFromResolver() {
+        activationService.activate(endpointMap);
+        verify(adapterResolver).getAdapter(ADAPTER);
+    }
+
+    @Test
+    public void activateGetsKoLocationFromRepo() {
+        activationService.activate(endpointMap);
+        verify(koRepo).getObjectLocation(ARK_ID);
+    }
+
+    @Test
+    public void activateCallsActivateOnAdapter() {
+        activationService.activate(endpointMap);
+        verify(adapter).activate(OBJECT_LOCATION, ARK_ID.getDashArkVersion(), ENDPOINT_NAME.substring(1), deploymentJson);
+    }
+
+    @Test
+    public void activateSetsExecutorInEndpointMap() {
+        activationService.activate(endpointMap);
+        verify(endpoint1).setExecutor(executor);
+    }
+
+    @Test
+    public void activateDoesNotSetExecutorIfActivatorExceptionIsThrownAnywhere() {
+        when(endpoint1.getDeployment()).thenReturn(null);
+        activationService.activate(endpointMap);
+        verify(endpoint1).setExecutor(null);
+    }
+
+    @Test
+    public void activateCatchesExceptionsFromShelf() {
+        when(koRepo.getObjectLocation(any())).thenThrow(new ShelfResourceNotFound("ope"));
+        activationService.activate(endpointMap);
+        verify(endpoint1).setExecutor(null);
+    }
+
+    @Test
+    public void activateCatchesExceptionsFromAdapter() {
+        when(adapter.activate(any(), any(), any(), any())).thenThrow(new AdapterException("ope"));
+        activationService.activate(endpointMap);
+        verify(endpoint1).setExecutor(null);
+    }
+
+    @Test
+    public void executeGetsExecutorFromEndpoint() {
+        activationService.execute(endpointId1, VERSION, "input");
+        verify(endpoint1).getExecutor();
+    }
+
+    @Test
+    public void executeExecutesExecutor() {
+        activationService.execute(endpointId1, VERSION, input);
+        verify(executor).execute(input);
+    }
+
+    @Test
+    public void executeSetsInputOnEndpointResult() {
+        EndPointResult result;
+        result = activationService.execute(endpointId1, VERSION, input);
+        assertEquals(input, result.getInfo().get("inputs"));
+    }
+
+    @Test
+    public void executeSetsMetadataOnEndpointResult() {
+        EndPointResult result = activationService.execute(endpointId1, VERSION, input);
+        assertEquals(metadata, result.getInfo().get("ko"));
+    }
+
+    @Test
+    public void executeThrowsActivatorExceptionWhenEndpointIsNotInMap() {
+        EndpointId missingId = new EndpointId(new ArkId("not", "in", "map"), "garbage");
+
+        ActivatorException activatorException = Assert.assertThrows(ActivatorException.class,
+                () -> {
+                    activationService.execute(
+                            missingId,
+                            VERSION, input);
+                });
+        assertEquals("No endpoint found for " + missingId, activatorException.getMessage());
+    }
+
+    @Test
+    public void executeThrowsActivatorExceptionWhenExecutorNotFound() {
+        when(endpoint1.getExecutor()).thenReturn(null);
+        ActivatorException activatorException = Assert.assertThrows(ActivatorException.class,
+                () -> {
+                    activationService.execute(
+                            endpointId1,
+                            VERSION, input);
+                });
+        assertEquals("No executor found for " + endpointId1, activatorException.getMessage());
+    }
+
+}
--- src/test/java/org/kgrid/activator/services/EndpointLoaderTests.java
+++ src/test/java/org/kgrid/activator/services/EndpointLoaderTests.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
 import static org.junit.Assert.*;
 import static org.kgrid.activator.utils.RepoUtils.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -47,19 +46,16 @@ public class EndpointLoaderTests {
     @Test
     public void endpointIsLoadedForAnImplementation() throws IOException {
 
-        // load single endpoint implementation
         Map<EndpointId, Endpoint> eps = endpointLoader.load(A_B_C);
         Endpoint endpoint = eps.get(new EndpointId(A_B_C, "/welcome"));
 
         assertNotNull("endpointPath 'a-b/c/welcome' should exist", endpoint);
 
-        // test that endpoint parts exists
         assertNotNull("service descriptor should exist", endpoint.getService());
         assertNotNull("deployment spec should exist", endpoint.getDeployment());
         assertNotNull("metadata should exist", endpoint.getMetadata());
 
-        // test deployment descriptor example
-        JsonNode deploymentSpec = getYamlTestFile(A_B_C.getDashArk() + "-" + A_B_C.getVersion(), "deployment.yaml");
+        JsonNode deploymentSpec = getYamlTestFile(A_B_C, "deployment.yaml");
 
         assertEquals("endpoint path exists in deployment descriptor",
                 endpoint.getDeployment().toString(),
@@ -68,12 +64,12 @@ public class EndpointLoaderTests {
         assertNotNull("enpoint spec 'a-b-c/info' is in original spec",
                 deploymentSpec.get("endpoints").get("/info"));
 
-        endpoint = eps.get(A_B_C.getDashArk() + "-" + A_B_C.getVersion() + "/info");
+        endpoint = eps.get(new EndpointId(A_B_C, "info"));
         assertNull("endpointPath 'a-b-c/info' should not exist", endpoint);
     }
 
     @Test
-    public void endpointIsLoadedForAnKO() throws IOException {
+    public void endpointIsLoadedForAnKO() {
 
         // load single endpoint implementation
         Map<EndpointId, Endpoint> eps = endpointLoader.load(C_D_F);
@@ -82,7 +78,6 @@ public class EndpointLoaderTests {
         assertEquals("should load 3 end points", 3, eps.size());
         assertNotNull("endpointPath 'c-d-f/welcome' should exist", endpoint);
 
-        // test that endpoint parts exists
         assertNotNull("service descriptor should exist", endpoint.getService());
         assertNotNull("deployment spec should exist", endpoint.getDeployment());
         assertNotNull("metadata should exist", endpoint.getMetadata());
@@ -90,12 +85,10 @@ public class EndpointLoaderTests {
     }
 
     @Test
-    public void activationPopulatesEndpoints() throws IOException {
+    public void activationPopulatesEndpointsWithMultipleVersions() {
 
-        // when
         Map<EndpointId, Endpoint> eps = endpointLoader.load();
 
-        // Loader methods load 2 KOs, with 3 impls, and 5 endpoints (1 service spec has 3 endpoints!)
         assertEquals("Map should have 5 endpoints", 6, eps.size());
 
         assertNotNull("'a-b-c/welcome' exists", eps.get(new EndpointId(A_B_C, "/welcome")));
@@ -109,28 +102,22 @@ public class EndpointLoaderTests {
     @Test
     public void endpointsContainServices() {
 
-        // when
         Map<EndpointId, Endpoint> endpoints = endpointLoader.load();
 
         endpoints.forEach((path, endpoint) -> {
             final JsonNode service = endpoint.getService();
             assertNotNull("Service spec must exist", service);
-            assertThat("Service spec must have endpoint path(service)",
-                    service.toString(),
-                    hasJsonPath("paths")
-            );
+            assertTrue("Service spec must have endpoint path(service)", service.has("paths"));
             String endpointKey = path.getEndpointName();
             assertNotNull("Service contains endpoint path", service.get("paths").get(endpointKey));
         });
     }
 
     @Test
-    public void shouldloadWhenOnlyServiceSpecHasextension() throws IOException {
-        // load single endpoint implementation
-        Map<EndpointId, Endpoint> eps = endpointLoader.load(TEST_SERVICE_EXTENSIONONLY);
+    public void shouldLoadWhenOnlyServiceSpecHasExtension() {
+        Map<EndpointId, Endpoint> eps = endpointLoader.load(TEST_SERVICE_EXTENSION_ONLY);
 
-        // test that endpoint should load with x-kgrid-activation extension only
-        Endpoint endpoint = eps.get(new EndpointId(TEST_SERVICE_EXTENSIONONLY, "/welcome"));
+        Endpoint endpoint = eps.get(new EndpointId(TEST_SERVICE_EXTENSION_ONLY, "/welcome"));
         assertEquals("V8", endpoint.getDeployment().get("adapter").asText());
     }
 
@@ -162,14 +149,14 @@ public class EndpointLoaderTests {
     @Test
     public void loadValidatesMetadata() throws IOException {
         endpointLoader.load(C_D_F);
-        JsonNode metadata = getJsonTestFile(C_D_F.getDashArk() + "-" + C_D_F.getVersion(), "metadata.json");
+        JsonNode metadata = getJsonTestFile(C_D_F, "metadata.json");
         verify(koValidationService, times(1)).validateMetadata(metadata);
     }
 
     @Test
     public void loadValidatesServiceSpec() throws IOException {
         endpointLoader.load(C_D_F);
-        JsonNode serviceSpec = getYamlTestFile(C_D_F.getDashArk() + "-" + C_D_F.getVersion(), "service.yaml");
+        JsonNode serviceSpec = getYamlTestFile(C_D_F, "service.yaml");
         verify(koValidationService, times(1)).validateServiceSpecification(serviceSpec);
     }
 
@@ -177,8 +164,8 @@ public class EndpointLoaderTests {
     @Test
     public void loadValidatesObjectForActivation() throws IOException {
         endpointLoader.load(C_D_F);
-        JsonNode serviceSpec = getYamlTestFile(C_D_F.getDashArk() + "-" + C_D_F.getVersion(), "service.yaml");
-        JsonNode deploymentSpec = getYamlTestFile(C_D_F.getDashArk() + "-" + C_D_F.getVersion(), "deployment.yaml");
+        JsonNode serviceSpec = getYamlTestFile(C_D_F, "service.yaml");
+        JsonNode deploymentSpec = getYamlTestFile(C_D_F, "deployment.yaml");
         verify(koValidationService, times(3)).validateActivatability(serviceSpec, deploymentSpec);
     }
 
@@ -186,53 +173,49 @@ public class EndpointLoaderTests {
      * Test loader methods
      */
     private void loadMockRepoWithKOs() throws IOException {
-        // All KOs on shelf (A_B, C_D)
         final Map<ArkId, JsonNode> kos = new HashMap<>();
-        kos.put(A_B_C, getJsonTestFile(A_B_C.getDashArk() + "-" + A_B_C.getVersion(), "metadata.json"));
-        kos.put(C_D_E, getJsonTestFile(C_D_E.getDashArk() + "-" + C_D_E.getVersion(), "metadata.json"));
-        kos.put(C_D_F, getJsonTestFile(C_D_F.getDashArk() + "-" + C_D_F.getVersion(), "metadata.json"));
-        kos.put(TEST_SERVICE_EXTENSIONONLY, getJsonTestFile(TEST_SERVICE_EXTENSIONONLY.getDashArk() + "-" + TEST_SERVICE_EXTENSIONONLY.getVersion(), "metadata.json"));
+        kos.put(A_B_C, getJsonTestFile(A_B_C, "metadata.json"));
+        kos.put(C_D_E, getJsonTestFile(C_D_E, "metadata.json"));
+        kos.put(C_D_F, getJsonTestFile(C_D_F, "metadata.json"));
+        kos.put(TEST_SERVICE_EXTENSION_ONLY, getJsonTestFile(TEST_SERVICE_EXTENSION_ONLY, "metadata.json"));
 
         given(repository.findAll()).willReturn(kos);
         given(repository.findKnowledgeObjectMetadata(C_D_E)).willReturn(
-                getJsonTestFile(C_D_E.getDashArk() + "-" + C_D_E.getVersion(), "metadata.json"));
+                getJsonTestFile(C_D_E, "metadata.json"));
     }
 
     private void loadMockRepoWithServiceSpecs() throws IOException {
-        //service specs
         given(repository.findServiceSpecification(eq(A_B_C), any()))
-                .willReturn(getYamlTestFile(A_B_C.getDashArk() + "-" + A_B_C.getVersion(), "service.yaml"));
+                .willReturn(getYamlTestFile(A_B_C, "service.yaml"));
         given(repository.findServiceSpecification(eq(C_D_E), any()))
-                .willReturn(getYamlTestFile(C_D_E.getDashArk() + "-" + C_D_E.getVersion(), "service.yaml"));
+                .willReturn(getYamlTestFile(C_D_E, "service.yaml"));
         given(repository.findServiceSpecification(eq(C_D_F), any()))
-                .willReturn(getYamlTestFile(C_D_F.getDashArk() + "-" + C_D_F.getVersion(), "service.yaml"));
-        given(repository.findServiceSpecification(eq(TEST_SERVICE_EXTENSIONONLY), any()))
-                .willReturn(getYamlTestFile(TEST_SERVICE_EXTENSIONONLY.getDashArk() + "-" + TEST_SERVICE_EXTENSIONONLY.getVersion(), "service.yaml"));
+                .willReturn(getYamlTestFile(C_D_F, "service.yaml"));
+        given(repository.findServiceSpecification(eq(TEST_SERVICE_EXTENSION_ONLY), any()))
+                .willReturn(getYamlTestFile(TEST_SERVICE_EXTENSION_ONLY, "service.yaml"));
     }
 
     private void loadMockRepoWithImplementations() throws IOException {
-        // implementations for those KOs
         given(repository.findKnowledgeObjectMetadata(eq(A_B_C)))
-                .willReturn(getJsonTestFile(A_B_C.getDashArk() + "-" + A_B_C.getVersion(), "metadata.json"));
+                .willReturn(getJsonTestFile(A_B_C, "metadata.json"));
         given(repository.findKnowledgeObjectMetadata(eq(C_D_E)))
-                .willReturn(getJsonTestFile(C_D_E.getDashArk() + "-" + C_D_E.getVersion(), "metadata.json"));
+                .willReturn(getJsonTestFile(C_D_E, "metadata.json"));
         given(repository.findKnowledgeObjectMetadata(eq(C_D_F)))
-                .willReturn(getJsonTestFile(C_D_F.getDashArk() + "-" + C_D_F.getVersion(), "metadata.json"));
-        given(repository.findKnowledgeObjectMetadata(eq(TEST_SERVICE_EXTENSIONONLY)))
-                .willReturn(getJsonTestFile(TEST_SERVICE_EXTENSIONONLY.getDashArk() + "-" + TEST_SERVICE_EXTENSIONONLY.getVersion(), "metadata.json"));
+                .willReturn(getJsonTestFile(C_D_F, "metadata.json"));
+        given(repository.findKnowledgeObjectMetadata(eq(TEST_SERVICE_EXTENSION_ONLY)))
+                .willReturn(getJsonTestFile(TEST_SERVICE_EXTENSION_ONLY, "metadata.json"));
 
     }
 
     private void loadMockRepoWithDeploymentSpecs() throws IOException {
-        // implementations for those KOs
         given(repository.findDeploymentSpecification(eq(A_B_C), any()))
-                .willReturn(getYamlTestFile(A_B_C.getDashArk() + "-" + A_B_C.getVersion(), "deployment.yaml"));
+                .willReturn(getYamlTestFile(A_B_C, "deployment.yaml"));
         given(repository.findDeploymentSpecification(eq(C_D_E), any()))
-                .willReturn(getYamlTestFile(C_D_E.getDashArk() + "-" + C_D_E.getVersion(), "deployment.yaml"));
+                .willReturn(getYamlTestFile(C_D_E, "deployment.yaml"));
         given(repository.findDeploymentSpecification(eq(C_D_F), any()))
-                .willReturn(getYamlTestFile(C_D_F.getDashArk() + "-" + C_D_F.getVersion(), "deployment.yaml"));
-        given(repository.findDeploymentSpecification(eq(TEST_SERVICE_EXTENSIONONLY), any()))
-                .willReturn(getYamlTestFile(TEST_SERVICE_EXTENSIONONLY.getDashArk() + "-" + TEST_SERVICE_EXTENSIONONLY.getVersion(), "service.yaml"));
+                .willReturn(getYamlTestFile(C_D_F, "deployment.yaml"));
+        given(repository.findDeploymentSpecification(eq(TEST_SERVICE_EXTENSION_ONLY), any()))
+                .willReturn(getYamlTestFile(TEST_SERVICE_EXTENSION_ONLY, "service.yaml"));
 
     }
 }
