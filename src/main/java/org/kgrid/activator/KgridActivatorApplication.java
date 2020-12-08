6de45eee043a4b52ba40065a5b4f23a6bb277031
ngittlen <nggittle@med.umich.edu>
2020-12-08 11:41:34 -0500

    Small security upgrades, removed special firewall handling and changed antmatcher to mvcmatcher to eliminate trailing slash loophole

--- src/main/java/org/kgrid/activator/KgridActivatorApplication.java
+++ src/main/java/org/kgrid/activator/KgridActivatorApplication.java
@@ -22,9 +22,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.firewall.DefaultHttpFirewall;
-import org.springframework.security.web.firewall.HttpFirewall;
-import org.springframework.security.web.firewall.StrictHttpFirewall;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -70,20 +67,12 @@ public class KgridActivatorApplication implements CommandLineRunner {
         activationController.activate();
     }
 
-    @Bean
-    public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
-        DefaultHttpFirewall firewall = new DefaultHttpFirewall();
-        firewall.setAllowUrlEncodedSlash(true);
-        return firewall;
-    }
-
     @Profile("dev")
     @Configuration
     public class DevWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         @Override
         public void configure(WebSecurity web) {
             web.ignoring().antMatchers("/**");
-            web.httpFirewall(allowUrlEncodedSlashHttpFirewall());
         }
     }
 
@@ -92,24 +81,19 @@ public class KgridActivatorApplication implements CommandLineRunner {
     public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         @Override
-        public void configure(WebSecurity web) {
-            web.httpFirewall(allowUrlEncodedSlashHttpFirewall());
-        }
-
-        @Override
         protected void configure(HttpSecurity http) throws Exception {
             http.csrf().disable()
                     .authorizeRequests()
                     .requestMatchers(EndpointRequest.to(HealthEndpoint.class)).authenticated()
                     .requestMatchers(EndpointRequest.to(InfoEndpoint.class)).authenticated()
-                    .antMatchers(HttpMethod.GET, "/activate").authenticated()
-                    .antMatchers(HttpMethod.POST, "/kos/manifest").authenticated()
-                    .antMatchers(HttpMethod.POST, "/kos/manifest-list").authenticated()
-                    .antMatchers(HttpMethod.POST, "/kos").authenticated()
-                    .antMatchers(HttpMethod.DELETE, "/kos/{naan}/{name}").authenticated()
-                    .antMatchers(HttpMethod.DELETE, "/kos/{naan}/{name}/{version}").authenticated()
-                    .antMatchers(HttpMethod.PUT, "/kos/{naan}/{name}").authenticated()
-                    .antMatchers(HttpMethod.PUT, "/kos/{naan}/{name}/{version}").authenticated()
+                    .mvcMatchers(HttpMethod.GET, "/activate").authenticated()
+                    .mvcMatchers(HttpMethod.POST, "/kos/manifest").authenticated()
+                    .mvcMatchers(HttpMethod.POST, "/kos/manifest-list").authenticated()
+                    .mvcMatchers(HttpMethod.POST, "/kos").authenticated()
+                    .mvcMatchers(HttpMethod.DELETE, "/kos/{naan}/{name}").authenticated()
+                    .mvcMatchers(HttpMethod.DELETE, "/kos/{naan}/{name}/{version}").authenticated()
+                    .mvcMatchers(HttpMethod.PUT, "/kos/{naan}/{name}").authenticated()
+                    .mvcMatchers(HttpMethod.PUT, "/kos/{naan}/{name}/{version}").authenticated()
                     .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
                     .and()
                     .httpBasic();
