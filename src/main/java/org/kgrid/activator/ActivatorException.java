451f2e3da26c7939412db29be21b9e30a9d2f474
mradamgbeck <mradamgbeck@gmail.com>
2020-12-24 15:11:03 -0500

    removed unused activator exception constructors

--- src/main/java/org/kgrid/activator/ActivatorException.java
+++ src/main/java/org/kgrid/activator/ActivatorException.java
@@ -6,40 +6,6 @@ public class ActivatorException extends RuntimeException {
 
   private HttpStatus status;
 
-  public ActivatorException() {
-    super();
-
-  }
-
-  public ActivatorException(String message, Throwable cause, boolean enableSuppression,
-      boolean writableStackTrace) {
-    super(message, cause, enableSuppression, writableStackTrace);
-
-  }
-
-  public ActivatorException(String message, Throwable cause) {
-    super(message, cause);
-
-  }
-
-  public ActivatorException(String message) {
-    super(message);
-
-  }
-
-  public ActivatorException(Throwable cause) {
-    super(cause);
-
-  }
-
-  public ActivatorException(String title, String message, Throwable cause) {
-
-  }
-
-  public ActivatorException(String title, String message) {
-
-  }
-
   public ActivatorException(String message, HttpStatus status){
     super(message);
     this.status = status;
@@ -53,6 +19,4 @@ public class ActivatorException extends RuntimeException {
   public HttpStatus getStatus(){
     return this.status;
   }
-
-
 }
