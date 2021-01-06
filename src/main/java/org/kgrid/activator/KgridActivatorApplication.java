73f9137b6cfc638258744b8dd98c5d9bc7f47f47
Adam G Beck <mradamgbeck@gmail.com>
2021-01-06 10:06:04 -0500

    Adapter loader test (#92)
    * refactored adapter loader test
    
    * removed comment from stack overflow

--- src/main/java/org/kgrid/activator/KgridActivatorApplication.java
+++ src/main/java/org/kgrid/activator/KgridActivatorApplication.java
@@ -69,7 +69,6 @@ public class KgridActivatorApplication implements CommandLineRunner {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        // Don't do this in production, use a proper list  of allowed origins
         config.setAllowedOrigins(Collections.singletonList("*"));
         config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
